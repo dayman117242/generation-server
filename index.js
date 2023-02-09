3e16f764d01f5d4633a75492d4ae62a51e1a5e4a
dayman117242 <111903443+dayman117242@users.noreply.github.com>
2023-02-09 01:36:46 +0000

    Updates

---
+++ index.js
@@ -0,0 +1,15 @@
+import { Configuration, OpenAIApi } from "openai";
+import {config} from 'dotenv';
+const configuration = new Configuration({
+    organization: "org-erz5K2MTl0F0rEYixUaPf6AI",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+    model: "text-davinci-003",
+  prompt: process.argv[2],
+  max_tokens: 100,
+  temperature: 0
+});
+
+console.log(response.data.choice[0].text)
